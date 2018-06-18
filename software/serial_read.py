6ceea6eac69614ea60534f276e61c099a3edd143
Andy Haas <drandyhaas@gmail.com>
2018-06-18 06:45:08 -0400

    wait a little longer, for setting up on bluetooth serial

--- software/serial_read.py
+++ software/serial_read.py
@@ -3,58 +3,59 @@ from struct import unpack
 import time
 
 ser=Serial("COM7",1500000,timeout=1.0)
+waitlittle = .1 #seconds
 
 #This is a _minimal_ set of example commands needed to send to a board to initialize it properly
 
 ser.write(chr(0)); ser.write(chr(20)) #set board id to 0
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(135)); ser.write(chr(0)); ser.write(chr(100)); #serialdelaytimerwait of 100
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(122)); ser.write(chr(0)); ser.write(chr(10)); #number of samples 10
-time.sleep(.01)
+time.sleep(waitlittle)
 
 ser.write(chr(123)); ser.write(chr(0)); #send increment
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(124)); ser.write(chr(3)); #downsample 3
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(125)); ser.write(chr(1)); #tickstowait 1
-time.sleep(.01)
+time.sleep(waitlittle)
 
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(32)); ser.write(chr(0)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # io expanders on (!)
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(32)); ser.write(chr(1)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # io expanders on (!)
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(33)); ser.write(chr(0)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # io expanders on (!)
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(33)); ser.write(chr(1)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # io expanders on (!)
-time.sleep(.01)
+time.sleep(waitlittle)
 
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(32)); ser.write(chr(18)); ser.write(chr(240)); ser.write(chr(255)); ser.write(chr(200)); # init
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(32)); ser.write(chr(19)); ser.write(chr(15)); ser.write(chr(255)); ser.write(chr(200)); # init (and turn on ADCs!)
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(33)); ser.write(chr(18)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # init
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(2)); ser.write(chr(33)); ser.write(chr(19)); ser.write(chr(0)); ser.write(chr(255)); ser.write(chr(200)); # init
-time.sleep(.01)
+time.sleep(waitlittle)
 
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(131)); ser.write(chr(8)); ser.write(chr(0)); # adc offset
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(131)); ser.write(chr(6)); ser.write(chr(16)); #offset binary output
 #ser.write(chr(131)); ser.write(chr(6)); ser.write(chr(80)); #test pattern output
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(131)); ser.write(chr(1)); ser.write(chr(0)); #not multiplexed
-time.sleep(.01)
+time.sleep(waitlittle)
 
 ser.write(chr(136)); ser.write(chr(3)); ser.write(chr(96)); ser.write(chr(80)); ser.write(chr(136)); ser.write(chr(22)); ser.write(chr(0)); # channel 0 , board 0 calib
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(3)); ser.write(chr(96)); ser.write(chr(82)); ser.write(chr(136)); ser.write(chr(22)); ser.write(chr(0)); # channel 1 , board 0 calib
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(3)); ser.write(chr(96)); ser.write(chr(84)); ser.write(chr(136)); ser.write(chr(22)); ser.write(chr(0)); # channel 2 , board 0 calib
-time.sleep(.01)
+time.sleep(waitlittle)
 ser.write(chr(136)); ser.write(chr(3)); ser.write(chr(96)); ser.write(chr(86)); ser.write(chr(136)); ser.write(chr(22)); ser.write(chr(0)); # channel 3 , board 0 calib
-time.sleep(.01)
+time.sleep(waitlittle)
 
 # OK, we're set up! Now we can read events and get good data out.
 
