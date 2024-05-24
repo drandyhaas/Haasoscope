d527bfaf8aa7c959ea260f710ab4e4afa4bcbd8d
Andy <drandyhaas@gmail.com>
2024-05-24 15:27:28 -0400

    can also control pin 12

--- software/serial_rebooter/serial_rebooter.ino
+++ software/serial_rebooter/serial_rebooter.ino
@@ -7,6 +7,10 @@ Processor: ATmega168
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  // pin 13
   digitalWrite(LED_BUILTIN,HIGH);
+
+  pinMode(12, OUTPUT);  // pin 12
+  digitalWrite(12,HIGH);
+
   Serial.begin(9600);
   Serial.println("Arduino Connected");
 }
@@ -17,6 +21,7 @@ void loop() {
     if(Serial.available()){
         input = Serial.readStringUntil('\n');
         Serial.println("Read: " + input);
+
         if(input.substring(0, 10) == "haasoscope") {
           if (input.substring(11,13) == "on") {
             Serial.println("on");
@@ -25,6 +30,28 @@ void loop() {
           else if (input.substring(11,14) == "off"){
             Serial.println("off");
             digitalWrite(LED_BUILTIN,LOW);
+          }
+        }
+
+        if(input.substring(0, 10) == "arduino_13") {
+          if (input.substring(11,13) == "on") {
+            Serial.println("on");
+            digitalWrite(13,HIGH);
+          }
+          else if (input.substring(11,14) == "off"){
+            Serial.println("off");
+            digitalWrite(13,LOW);
+          }
+        }
+
+        if(input.substring(0, 10) == "arduino_12") {
+          if (input.substring(11,13) == "on") {
+            Serial.println("on");
+            digitalWrite(12,HIGH);
+          }
+          else if (input.substring(11,14) == "off"){
+            Serial.println("off");
+            digitalWrite(12,LOW);
           }
         }
     }
