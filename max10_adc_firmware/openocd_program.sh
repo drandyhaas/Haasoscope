fc88544058ba02665b867413e07266015d1eaec7
drandyhaas <drandyhaas@gmail.com>
2026-06-05 21:13:05 -0400

    note on openocd

--- max10_adc_firmware/openocd_program.sh
+++ max10_adc_firmware/openocd_program.sh
@@ -1,2 +1,4 @@
 openocd -f ./haasoscope.cfg -c "svf /tmp/serial1.svf ; exit"
 
+# This worked on my mac:
+# ! sudo openFPGALoader -c usb-blaster -f /Users/ahaas/PycharmProjects/Haasoscope/max10_adc_firmware/output_files/serial1.pof
